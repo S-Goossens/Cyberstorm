7d0c88e5237543f5075b8ba034d5ce9277174c07
Sidney Goossens <45096509+S-Goossens@users.noreply.github.com>
2021-01-22 16:51:38 +0100

    small changes

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { OrderListItemComponent } from './order-list/order-list-item/order-list-item.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 
 
 @NgModule({
-  declarations: [OrdersComponent, OrderListComponent, OrderListItemComponent],
+  declarations: [OrdersComponent, OrderListComponent, OrderListItemComponent, OrderDetailComponent],
   imports: [
     CommonModule
   ]
